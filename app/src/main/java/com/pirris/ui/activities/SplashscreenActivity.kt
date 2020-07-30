01ebdafd79f8bd8130436b811b4c4cbdd1f6255b
Pirris <luissalas_10@outlook.com>
2020-07-29 21:51:47 -0600

    Se crea el activity SplashScreen, los fragments home-Schedule-speakers-ubication así como los dialog fragments también

---
+++ app/src/main/java/com/pirris/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package com.pirris.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pirris.R
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
