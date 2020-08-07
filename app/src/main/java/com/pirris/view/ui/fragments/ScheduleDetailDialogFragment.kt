3826af1b10f5196c567c1ee912b74d0f2f93ece5
Pirris <luissalas_10@outlook.com>
2020-08-07 16:15:59 -0600

    En este código se establece la funcionalidad del fragment_schedule_detail_dialog, en la cual traemos el objeto/valores desde fragment_schedule.xml y colocamos las variables en cada view. Además se crea un tema para no afectar el diseño del layout

--- app/src/main/java/com/pirris/view/ui/fragments/ScheduleDetailDialogFragment.kt
+++ app/src/main/java/com/pirris/view/ui/fragments/ScheduleDetailDialogFragment.kt
@@ -1,11 +1,17 @@
 package com.pirris.view.ui.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
 import com.pirris.R
+import com.pirris.model.Conference
+import kotlinx.android.synthetic.main.fragment_schedule_detail_dialog.*
+import java.text.SimpleDateFormat
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,44 +23,49 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ScheduleDetailDialogFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ScheduleDetailDialogFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class ScheduleDetailDialogFragment : DialogFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialogStyle)
     }
 
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_schedule_detail_dialog, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ScheduleDetailDialogFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ScheduleDetailDialogFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        toolbarConference.navigationIcon = ContextCompat.getDrawable(view.context, R.drawable.ic_close)
+        toolbarConference.setTitleTextColor(Color.WHITE)
+        toolbarConference.setNavigationOnClickListener {
+            dismiss()
+        }
+
+        //Con esta línea traemos los datos del fragment fragment_schedule.xml y lo convertimos a objeto
+        val conference = arguments?.getSerializable("conference") as Conference
+
+        toolbarConference.title = conference.title
+
+        tvItemScheduleTituloConferencia.text = conference.title
+
+        val pattern = "dd/MM/yyyy hh:mm a"
+        val simpleDF = SimpleDateFormat(pattern)
+        val date = simpleDF.format(conference.datetime)
+        tvDetailConferenceHour.text = date
+
+        tvDetailConferenceSpeaker.text = conference.speaker
+        tvDetailConferenceTag.text = conference.tag
+        tvDetailConferenceDescription.text = conference.description
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
     }
 }
